6be06477bf1585acf2b88a2b43bf3bf83365fac7
Yan Minagawa <t@c-base.org>
2012-07-10 17:55:23 +0200

    add unixbench

--- bb.sh
+++ bb.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 apt-get update
 apt-get install vim screen build-essential
+wget http://byte-unixbench.googlecode.com/files/UnixBench5.1.3.tgz
+tar xfzv UnixBench5.1.3.tgz
+cd UnixBench
+./Run
